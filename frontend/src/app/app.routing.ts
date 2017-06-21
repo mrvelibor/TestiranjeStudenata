e7316d716fdbffa428d1862fffb2ceb52b14c6ca
Velibor Bacujkov <mr.velibor@gmail.com>
2017-06-21 22:09:12 +0200

    Small changes

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -2,13 +2,11 @@
 
 import {LoginComponent} from './pages/main-panel/login/login.component';
 import {HomeComponent} from "./pages/main-panel/home/home.component";
-import {AuthGuard} from "./guards/auth.guard";
 import {AvailableExamsComponent} from "./pages/student-panel/available-exams/available-exams.component";
 import {ExamTestComponent} from "./pages/student-panel/exam-test/exam-test.component";
 import {ExamOverviewComponent} from "./pages/profesor-panel/exam-overview/exam-overview.component";
 import {StudentGuard} from "./guards/student.guard";
 import {ProfesorGuard} from "./guards/profesor.guard";
-import {CompletedExamListComponent} from "./pages/profesor-panel/completed-exam-list/completed-exam-list.component";
 import {CompletedExamsComponent} from "./pages/student-panel/completed-exams/completed-exams.component";
 import {CourseEditorComponent} from "./pages/admin-panel/course-editor/course-editor.component";
 import {AdminGuard} from "./guards/admin.guard";
