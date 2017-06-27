2232a752c24d3e739ab651eb7dfc0ab64f6cf63b
Velibor Bacujkov <mr.velibor@gmail.com>
2017-06-28 00:34:27 +0200

    Add single choice question

--- backend/src/main/java/com/mrvelibor/testiranjestudenata/rest/ExamRestController.java
+++ backend/src/main/java/com/mrvelibor/testiranjestudenata/rest/ExamRestController.java
@@ -3,13 +3,17 @@ package com.mrvelibor.testiranjestudenata.rest;
 import com.mrvelibor.testiranjestudenata.data.entities.Exam;
 import com.mrvelibor.testiranjestudenata.data.entities.MultipleChoiceAnswer;
 import com.mrvelibor.testiranjestudenata.data.entities.Question;
+import com.mrvelibor.testiranjestudenata.data.entities.SingleChoiceAnswer;
 import com.mrvelibor.testiranjestudenata.data.repository.ExamRepository;
 import com.mrvelibor.testiranjestudenata.data.repository.QuestionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
+import java.util.Collection;
 import java.util.List;
 
 @RestController()
@@ -49,41 +53,64 @@ public class ExamRestController {
         if(exam == null) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-        final Question question = new Question();
+        if(questionJson.getText() == null || questionJson.getText().isEmpty() || questionJson.getQuestionType() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        Question question = new Question();
         question.setExam(exam);
         question.setText(questionJson.getText());
-        question.setImageUrl(questionJson.getImageUrl());
         question.setQuestionType(questionJson.getQuestionType());
         switch (question.getQuestionType()) {
-            case truefalse:
-                if(questionJson.getCorrectStatement() == null) {
+            case truefalse: {
+                Boolean correctStatement = questionJson.getCorrectStatement();
+                if (correctStatement == null) {
                     return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
                 }
-                question.setCorrectStatement(questionJson.getCorrectStatement());
+                question.setCorrectStatement(correctStatement);
                 break;
-            case numerical:
-                if(questionJson.getCorrectValue() == null) {
+            }
+            case numerical: {
+                BigDecimal correctValue = questionJson.getCorrectValue();
+                if (correctValue == null) {
                     return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
                 }
-                question.setCorrectValue(questionJson.getCorrectValue());
+                question.setCorrectValue(correctValue);
                 break;
-            case single:
-                if(questionJson.getSingleChoiceAnswers() == null || questionJson.getSingleChoiceAnswers().size() < 3) {
+            }
+            case single: {
+                SingleChoiceAnswer correctAnswer = questionJson.getSingleChoiceCorrectAnswer();
+                Collection<SingleChoiceAnswer> answers = questionJson.getSingleChoiceAnswers();
+                if (correctAnswer == null || correctAnswer.getSingleChoiceAnswerId() == null || answers == null || answers.size() < 3) {
                     return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
                 }
-                question.setSingleChoiceAnswers(questionJson.getSingleChoiceAnswers());
+                if (answers.stream().anyMatch(a -> a.getText() == null || a.getText().isEmpty())) {
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                }
+                SingleChoiceAnswer answer = answers.stream().filter(a -> a.getSingleChoiceAnswerId() == correctAnswer.getSingleChoiceAnswerId()).findAny().get();
+                answers.forEach(a -> {
+                    a.setQuestion(question);
+                    a.setSingleChoiceAnswerId(null);
+                });
+                question.setSingleChoiceAnswers(answers);
+                question.setSingleChoiceCorrectAnswer(answer);
                 break;
-            case multiple:
-                if(questionJson.getMultipleChoiceAnswers() == null || questionJson.getMultipleChoiceAnswers().size() < 3) {
+            }
+            case multiple: {
+                Collection<MultipleChoiceAnswer> answers = questionJson.getMultipleChoiceAnswers();
+                if (answers == null || answers.size() < 3) {
                     return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
                 }
-                questionJson.getMultipleChoiceAnswers().stream().filter(answer -> answer.getAnswerCorrect() == null).forEach(answer -> answer.setAnswerCorrect(false));
-                if(questionJson.getMultipleChoiceAnswers().stream().noneMatch(MultipleChoiceAnswer::getAnswerCorrect)) {
+                answers.stream().filter(a -> a.getAnswerCorrect() == null).forEach(a -> a.setAnswerCorrect(false));
+                if (answers.stream().noneMatch(MultipleChoiceAnswer::getAnswerCorrect)) {
                     return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
                 }
-                questionJson.getMultipleChoiceAnswers().forEach(answer -> answer.setQuestion(question));
-                question.setMultipleChoiceAnswers(questionJson.getMultipleChoiceAnswers());
+                if (answers.stream().anyMatch(a -> a.getText() == null || a.getText().isEmpty())) {
+                    return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                }
+                answers.forEach(answer -> answer.setQuestion(question));
+                question.setMultipleChoiceAnswers(answers);
                 break;
+            }
             default:
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
